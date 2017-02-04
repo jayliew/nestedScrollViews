ca4ded85dd7cae0b626f21875213004bd6c7074a
Jay Liew <jaysern@gmail.com>
2017-02-04 15:46:22 -0800

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nested ScrollViews
+
+Demo of 2 UIScrollViews nested with a Container View, scrolling in different directions (horizontal and vertical). Written in Swift
+
+![Demo](nestedScrollViews.gif)
--- nestedScrollViews/ViewController.swift
+++ nestedScrollViews/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet var outerScroll: UIScrollView!
+    @IBOutlet var outerContainer: UIView!
+    @IBOutlet var bottomOuterView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+        
+        outerScroll.delegate = self
+        
+        outerScroll.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height * 2)
+        
+    } // viewDidLoad
+    
+    
+    /*
+     override func viewDidLoad() {
+     super.viewDidLoad()
+     
+     //automaticallyAdjustsScrollViewInsets = false
+     
+     //        outerScroll.delegate = self
+     //        innerScroll.delegate = self
+     
+     // image is 1024 by 669
+     
+     let imageHeight = imageView.image!.size.height
+     let imageWidth = imageView.image!.size.width
+     
+     let innerContentheight = view.frame.height / 2
+     
+     let scaleFactor = imageHeight / innerContentheight
+     let innerContentWidth = imageWidth / scaleFactor
+     
+     innerScroll.contentSize = CGSize(width: imageWidth, height: imageHeight)
+     
+     
+     let widthScale = outerContainer.bounds.width / imageView.bounds.width
+     let heightScale = (outerContainer.bounds.height / 2) / imageView.bounds.height
+     let minScale = min(widthScale, heightScale)
+     
+     innerScroll.minimumZoomScale = minScale
+     innerScroll.zoomScale = minScale
+     
+     
+     
+     //innerScroll.contentSize = CGSize(width: innerContentWidth, height: innerContentheight)
+     
+     //let newImageFrame = CGRect(x: imageView.frame.origin.x, y: imageView.frame.origin.y, width: innerContentWidth, height: innerContentheight)
+     
+     //        let newImageFrame = CGRect(x: 0, y: 0, width: innerContentWidth, height: innerContentheight)
+     //
+     //        imageView.frame = newImageFrame
+     //
+     
+     
+     }//viewDidLoad
+     
+     */
+    
 }
 
